596137abf1844c00fc5716393fd4e700f2658722
darren <764190362@qq.com>
2017-10-26 16:15:40 +0800

    设置路由规则

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" activ-class="active" exact><a>Home</a></router-link>
-        <router-link to="/user" tag="li"><a>User</a></router-link>
+        <router-link to="/user/10" tag="li"><a>User</a></router-link>
     </ul>
 </template>
