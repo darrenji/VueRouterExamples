ce51d9d6d7c904d914776109505e07d1ab27549a
darren <764190362@qq.com>
2017-10-26 13:27:05 +0800

    搭好框架

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
