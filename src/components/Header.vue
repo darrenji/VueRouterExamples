e0a1fe6e38f1aa71e9d89446ca0f6a050e7a55a4
darren <764190362@qq.com>
2017-10-26 16:45:07 +0800

    追踪路由中的参数变化

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" activ-class="active" exact><a>Home</a></router-link>
-        <router-link to="/user/10" tag="li"><a>User</a></router-link>
+        <router-link to="/user/1" tag="li"><a>User1</a></router-link>
+        <router-link to="/user/2" tag="li"><a>User2</a></router-link>
     </ul>
 </template>
