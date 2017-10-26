2c4919dfb4c75b7e08c6c0e3bb63a2ec8e842d2f
darren <764190362@qq.com>
2017-10-26 16:51:24 +0800

    追中路由中的参数变化1

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>The Use Page</h1>
         <hr>
-        <p>Loaded ID: {{id}}</p>
+        <p>Loaded ID is: {{id}}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
