f67cfb8e5d4c63d0556e14820a68cf86560c85ec
darren <764190362@qq.com>
2017-10-26 16:06:39 +0800

    链接样式的小技巧

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="nav nav-pills">
-        <li role="presentation"><router-link to="/">Home</router-link></li>
-        <li role="presentation"><router-link to="/user">User</router-link></li>
+        <router-link to="/" tag="li" activ-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user" tag="li"><a>User</a></router-link>
     </ul>
 </template>
