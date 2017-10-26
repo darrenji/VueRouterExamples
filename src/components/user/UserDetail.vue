0e05728a2b89a9c79c0421a144b476aa2b0b5eb5
darren <764190362@qq.com>
2017-10-26 17:38:39 +0800

    把路由参数放到router-link中

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,5 +2,6 @@
     <div>
         <h1>The Use Detail Page</h1>
         <p>User loaded has id: {{$route.params.id}}</p>
+        <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
