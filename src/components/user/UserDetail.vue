2493636ed6a30cd9645dc9abeb183b0f43511d53
darren <764190362@qq.com>
2017-10-26 20:23:56 +0800

    路由的beforeLeave

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -21,15 +21,6 @@
                     hash: '#data'
                 }
             }
-        },
-        beforeRouteEnter(to, from, next){
-            //在外面不能获取this.link
-            //这样获取：next(vm => {vm.link});
-//            if(true){
-//                next();
-//            } else {
-//                next(false);
-//            }
         }
     }
 </script>
